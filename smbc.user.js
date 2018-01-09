af6d213a3a66a0e3a8b849020828b2b5f5f56f1a
Asral86 <larsm.andersson@gmail.com>
2018-01-09 19:07:14 +0100

    smbc
    smbc

---
+++ smbc.user.js
@@ -0,0 +1,14 @@
+﻿// ==UserScript==
+// @name         SMBC
+// @description  Title-text under bilden
+// @author       Lars Andersson
+// @version      1.0
+// @include      https://www.smbc-comics.com/*
+// @include      https://smbc-comics.com/*
+// @grant        none
+// ==/UserScript==
+
+var thetitle = document.querySelector('#cc-comic').getAttribute('title');
+var thep = document.createElement('p');
+thep.textContent = thetitle;
+document.getElementById('cc-comicbody').appendChild(thep);
