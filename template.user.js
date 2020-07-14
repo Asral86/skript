feab3099a82a1a6cb9079862374fc98aeacf711c
Asral86 <larsm.andersson@gmail.com>
2020-07-14 17:33:12 +0200

    Template för nya skript...

---
+++ template.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         
+// @description  
+// @version      1.0
+// @author       Lars Andersson
+// @namespace    https://github.com/Asral86/skript
+// @match        
+// @grant        none
+// ==/UserScript==
+
+// Template för nya skript, lägg till kod här...
