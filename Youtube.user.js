9380edeb9fccdaba366fe451fb42eda253fca8b4
Asral86 <larsm.andersson@gmail.com>
2021-02-09 19:34:20 +0100

    Youtube 1.0.2
    bugfix

--- Youtube.user.js
+++ Youtube.user.js
@@ -2,7 +2,7 @@
 // @name         Youtube
 // @description  Länka till video utan playlist på playlist-sidor
 // @author       Lars Andersson
-// @version      1.0.1
+// @version      1.0.2
 // @include      https://www.youtube.com/playlist*
 // @grant        none
 // @run-at       document-idle
@@ -14,11 +14,11 @@ function createElement(type, attributes) {
 	return node;
 }
 
-var videor = document.querySelectorAll('div#contents > ytd-playlist-video-renderer > div#content > a[href*="/watch"]');
+var videor = document.querySelectorAll('ytd-playlist-video-renderer > div#content #meta a[href*="/watch"]');
 console.log("Hej! "+videor.length);
 for (var i=0; i<videor.length; i++) {
   var lnk = videor[i].href.split('&',1)[0];
   var thea = createElement('a', {href: lnk, class: 'yt-simple-endpoint style-scope yt-formatted-string', style: 'white-space: unset'});
   thea.textContent = ' | Spela upp utan playlist';
-  document.querySelectorAll('div#contents > ytd-playlist-video-renderer > div#content div#meta h3 + ytd-video-meta-block #byline-container > ytd-channel-name yt-formatted-string#text')[i].appendChild(thea);
+  document.querySelectorAll('ytd-playlist-video-renderer > div#content #meta h3 + ytd-video-meta-block #byline-container > ytd-channel-name yt-formatted-string#text')[i].appendChild(thea);
 }
