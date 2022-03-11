0bdd0f6e9b00d75e95304ebe8a23c0144afaaaae
Asral86 <larsm.andersson@gmail.com>
2022-03-11 17:37:31 +0100

    Youtube 1.1
    + knapp för att mörda playlist på videosidor med playlists

--- Youtube.user.js
+++ Youtube.user.js
@@ -1,12 +1,15 @@
 ﻿// ==UserScript==
 // @name         Youtube
-// @description  Länka till video utan playlist på playlist-sidor
+// @description  Länka till video utan playlist
 // @author       Lars Andersson
-// @version      1.0.2
+// @version      1.1
 // @include      https://www.youtube.com/playlist*
+// @include      https://www.youtube.com/watch?v=*&list=*
 // @grant        none
 // @run-at       document-idle
 // ==/UserScript==
+
+var url = document.URL;
 
 function createElement(type, attributes) {
 	var node = document.createElement(type);
@@ -14,11 +17,27 @@ function createElement(type, attributes) {
 	return node;
 }
 
-var videor = document.querySelectorAll('ytd-playlist-video-renderer > div#content #meta a[href*="/watch"]');
-console.log("Hej! "+videor.length);
-for (var i=0; i<videor.length; i++) {
-  var lnk = videor[i].href.split('&',1)[0];
-  var thea = createElement('a', {href: lnk, class: 'yt-simple-endpoint style-scope yt-formatted-string', style: 'white-space: unset'});
-  thea.textContent = ' | Spela upp utan playlist';
-  document.querySelectorAll('ytd-playlist-video-renderer > div#content #meta h3 + ytd-video-meta-block #byline-container > ytd-channel-name yt-formatted-string#text')[i].appendChild(thea);
+if (url.includes("/playlist") == true) {
+	function fixit() {
+		var videor = document.querySelectorAll('ytd-playlist-video-renderer > div#content #meta a[href*="/watch"]');
+		console.log("Hej! "+videor.length);
+		for (var i=0; i<videor.length; i++) {
+			var lnk = videor[i].href.split('&',1)[0];
+			var thea = createElement('a', {href: lnk, class: 'yt-simple-endpoint style-scope yt-formatted-string', style: 'white-space: unset'});
+			thea.textContent = ' | Spela upp utan playlist';
+			document.querySelectorAll('ytd-playlist-video-renderer > div#content #meta h3 + ytd-video-meta-block #byline-container > ytd-channel-name yt-formatted-string#text')[i].appendChild(thea);
+		}
+	}
+	window.setTimeout(fixit, 1000);
+}
+
+else if (url.includes("/watch?v=") == true) {
+	var link = url.split('&list')[0];
+	var kill = createElement('a', { href: link, style: 'position: fixed; bottom: 8px; right: 8px; \
+		background-color: var(--yt-spec-brand-button-background); color: var(--yt-spec-static-brand-white); \
+		border-radius: 2px; padding: var(--yt-button-padding); \
+		font-size: var(--ytd-tab-system_-_font-size); font-weight: var(--ytd-tab-system_-_font-weight); letter-spacing: var(--ytd-tab-system_-_letter-spacing); \
+		text-decoration: none; text-transform: var(--ytd-tab-system_-_text-transform);' });
+	kill.textContent = "Mörda playlist";
+	document.body.appendChild(kill);
 }
