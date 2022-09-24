ea71f0f4f12138646dc2d44f72793bf8fceb4691
Asral86 <larsm.andersson@gmail.com>
2022-09-24 22:35:13 +0200

    Ars 1.1.1

--- arstechnica.user.js
+++ arstechnica.user.js
@@ -1,19 +1,19 @@
 ﻿// ==UserScript==
 // @name         Arstechnica
-// @description  Snyggare tid i bylines
+// @description  Snyggare tid i bylines, forumgrejs
 // @author       Lars Andersson
-// @version      1.1
+// @version      1.1.1
 // @include      https://arstechnica.com/*
 // @grant        none
 // @run-at       document-idle
 // ==/UserScript==
 
 if (document.body.classList.contains('single-post') === true) {
-  var datum = document.querySelector('.article-header .byline time'); //console.log(datum);
-  var datte = new Date(datum.getAttribute('datetime')); //console.log(datte);
-  var stajl = { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' };
-  datum.setAttribute('title', datum.textContent);
-  datum.textContent = datte.toLocaleString('sv-SE', stajl); 
+	var datum = document.querySelector('.article-header .byline time'); //console.log(datum);
+	var datte = new Date(datum.getAttribute('datetime')); //console.log(datte);
+	var stajl = { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' };
+	datum.setAttribute('title', datum.textContent);
+	datum.textContent = datte.toLocaleString('sv-SE', stajl); 
 }
 
 else if (document.URL.includes('/civis/viewtopic.php') == true) {
