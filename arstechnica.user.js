5bf63bd318a93d58f471d208653b97286d44fdc6
Asral86 <larsm.andersson@gmail.com>
2022-11-11 01:04:08 +0100

    Ars 1.2
    rm kod för gamla forumet

--- arstechnica.user.js
+++ arstechnica.user.js
@@ -1,8 +1,8 @@
 ﻿// ==UserScript==
 // @name         Arstechnica
-// @description  Snyggare tid i bylines, forumgrejs
+// @description  Snyggare tid i bylines
 // @author       Lars Andersson
-// @version      1.1.1
+// @version      1.2
 // @include      https://arstechnica.com/*
 // @grant        none
 // @run-at       document-idle
@@ -14,16 +14,4 @@ if (document.body.classList.contains('single-post') === true) {
 	var stajl = { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' };
 	datum.setAttribute('title', datum.textContent);
 	datum.textContent = datte.toLocaleString('sv-SE', stajl); 
-}
-
-else if (document.URL.includes('/civis/viewtopic.php') == true) {
-	if (!document.getElementById('ull')) {
-		var mittskit = document.querySelector('#userbar li.user-threads').cloneNode(true);
-		var ull = document.createElement('ul');
-		ull.classList.add('list-inline');
-		ull.setAttribute('style','margin-top: 0.5em;');
-		ull.id = 'ull';
-		ull.appendChild(mittskit);
-		document.querySelector('#bottom-reply + nav.breadcrumb').appendChild(ull);
-	}
 }
