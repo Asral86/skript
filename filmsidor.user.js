b267b22b84a7c9bff8e8596bba3fe0519c4e24e3
Asral86 <larsm.andersson@gmail.com>
2023-11-23 19:50:28 +0100

    filmsidor 1.7
    * cleanup steg 2: fixa formatering

--- filmsidor.user.js
+++ filmsidor.user.js
@@ -2,7 +2,7 @@
 // @name         Filmsidor
 // @description  Länka ihop Filmtipset, Imdb, Cinemageddon, Letterboxd
 // @author       Lars Andersson
-// @version      1.6.9
+// @version      1.7
 // @match        *://www.filmtipset.se/film/*
 // @match        *://*.imdb.com/title/*
 // @match        *://cinemageddon.net/details.php?*
@@ -11,33 +11,33 @@
 // ==/UserScript==
 
 function createElement(type, attributes) {
-  var node = document.createElement(type);
-  for (var attr in attributes) if (attributes.hasOwnProperty(attr)) { node.setAttribute(attr, attributes[attr]); }
-  return node;
+	var node = document.createElement(type);
+	for (var attr in attributes) if (attributes.hasOwnProperty(attr)) { node.setAttribute(attr, attributes[attr]); }
+	return node;
 }
 
 var url = document.URL;
 
 if (url.includes('www.filmtipset.se') == true) {
-  var imdb   = document.querySelector('div.content a[href*="imdb.com/title/"]');
-  var imdb2  = imdb.getAttribute('href').split('/title/');
-  var imdbid = imdb2[1]; //console.log(imdbid.length);
-  if (imdbid.length > 9 && imdbid.includes('tt0') == true) { // Ta bort extra nollor i imdb-id
-    for (var i=imdbid.length; i>9; i--) { imdbid = imdbid.replace('tt0', 'tt'); }
-  }
-  imdb.href = "https://www.imdb.com/title/" + imdbid;
-  var cglink = createElement('a', {href: 'https://cinemageddon.net/browse.php?search=' + imdbid, target: '_blank', rel: 'noopener noreferrer'}); cglink.textContent = "Cinemageddon";
-  var lblink = createElement('a', {href: 'https://letterboxd.com/imdb/' + imdbid, target: '_blank', rel: 'noopener noreferrer'}); lblink.textContent = "Letterboxd";
-  var span1  = createElement('span', {class: 'postmeta hideMobile'});
-  var span2  = createElement('span', {class: 'postmeta hideMobile'});
-  var ikon1  = createElement('i', {class: 'fa fa-film'});
-  var ikon2  = createElement('i', {class: 'fa fa-film'});
-  var enbr   = createElement('br');
-  span1.appendChild(ikon1); span1.appendChild(lblink);
-  span2.appendChild(ikon2); span2.appendChild(cglink);
-  imdb.parentNode.insertAdjacentElement('afterend', span1);
-  imdb.parentNode.insertAdjacentElement('afterend', span2);
-  imdb.parentNode.insertAdjacentElement('beforebegin', enbr);
+	var imdb   = document.querySelector('div.content a[href*="imdb.com/title/"]');
+	var imdb2  = imdb.getAttribute('href').split('/title/');
+	var imdbid = imdb2[1]; //console.log(imdbid.length);
+	if (imdbid.length > 9 && imdbid.includes('tt0') == true) { // Ta bort extra nollor i imdb-id
+		for (var i=imdbid.length; i>9; i--) { imdbid = imdbid.replace('tt0', 'tt'); }
+	}
+	imdb.href = "https://www.imdb.com/title/" + imdbid;
+	var cglink = createElement('a', {href: 'https://cinemageddon.net/browse.php?search=' + imdbid, target: '_blank', rel: 'noopener noreferrer'}); cglink.textContent = "Cinemageddon";
+	var lblink = createElement('a', {href: 'https://letterboxd.com/imdb/' + imdbid, target: '_blank', rel: 'noopener noreferrer'}); lblink.textContent = "Letterboxd";
+	var span1  = createElement('span', {class: 'postmeta hideMobile'});
+	var span2  = createElement('span', {class: 'postmeta hideMobile'});
+	var ikon1  = createElement('i', {class: 'fa fa-film'});
+	var ikon2  = createElement('i', {class: 'fa fa-film'});
+	var enbr   = createElement('br');
+	span1.appendChild(ikon1); span1.appendChild(lblink);
+	span2.appendChild(ikon2); span2.appendChild(cglink);
+	imdb.parentNode.insertAdjacentElement('afterend', span1);
+	imdb.parentNode.insertAdjacentElement('afterend', span2);
+	imdb.parentNode.insertAdjacentElement('beforebegin', enbr);
 }
 
 else if (url.includes('imdb.com') == true) {
@@ -66,27 +66,27 @@ else if (url.includes('imdb.com') == true) {
 }
 
 else if (url.includes('cinemageddon.net') == true) {
-  document.title = document.title.replace('cinemageddon // torrent details for "', ' ').slice(0,-1) + " // cinemageddon";
-  if (document.querySelector('#altlist_row + tr + tr + tr td.rowhead').textContent == "IMDB") {
-    var imdb = document.querySelectorAll('#altlist_row + tr + tr + tr a[href^="http://www.imdb.com/title/"]'); }
-  else { var imdb = document.querySelectorAll('#altlist_row + tr + tr + tr + tr a[href^="http://www.imdb.com/title/"]'); }
-  if ( imdb[0].textContent != "" ) {
-    for (var i=0; i<=imdb.length-1; i++) {
-      if (imdb[i].classList.contains('lb') !== true) {
+	document.title = document.title.replace('cinemageddon // torrent details for "', ' ').slice(0,-1) + " // cinemageddon";
+	if (document.querySelector('#altlist_row + tr + tr + tr td.rowhead').textContent == "IMDB") {
+		var imdb = document.querySelectorAll('#altlist_row + tr + tr + tr a[href^="http://www.imdb.com/title/"]'); }
+	else { var imdb = document.querySelectorAll('#altlist_row + tr + tr + tr + tr a[href^="http://www.imdb.com/title/"]'); }
+	if ( imdb[0].textContent != "" ) {
+		for (var i=0; i<=imdb.length-1; i++) {
+			if (imdb[i].classList.contains('lb') !== true) {
 				imdb[i].classList.add('lb');
-      	var lb = createElement('a', {href: 'https://letterboxd.com/imdb/' + imdb[i].textContent, target: '_blank', rel: 'noopener noreferrer'});
-      	lb.textContent = ' (lb)';
-      	imdb[i].parentNode.insertBefore(lb, imdb[i].nextSibling);
-      }
-    }
-  }
-  // Auto-expandera bilder i beskrivningar
-  var imgs = document.querySelectorAll('#torrent_details + table.frames img[title="Click to see original size"]');
-  if (imgs.length > 0) {
+				var lb = createElement('a', {href: 'https://letterboxd.com/imdb/' + imdb[i].textContent, target: '_blank', rel: 'noopener noreferrer'});
+				lb.textContent = ' (lb)';
+				imdb[i].parentNode.insertBefore(lb, imdb[i].nextSibling);
+			}
+		}
+	}
+	// Auto-expandera bilder i beskrivningar
+	var imgs = document.querySelectorAll('#torrent_details + table.frames img[title="Click to see original size"]');
+	if (imgs.length > 0) {
 		for (var y = 0; y <= imgs.length; y++) {
 			if (imgs[y].hasAttribute('width')) imgs[y].removeAttribute('width');
 		}
-  }
+	}
 }
 
 else if (url.includes('https://letterboxd.com/film/') == true) {
