628d54364bea04adaccbcf7ceb5c28df712984d7
Asral86 <larsm.andersson@gmail.com>
2024-01-27 00:25:59 +0100

    svtplay 1.0.7
    Fix: klassnamn har ändrats igen

--- svtplay.user.js
+++ svtplay.user.js
@@ -2,7 +2,7 @@
 // @name         SVT Play
 // @description  Lägg till slutdatum i titel
 // @author       Lars Andersson
-// @version      1.0.6
+// @version      1.0.7
 // @include      https://www.svtplay.se/video/*
 // @grant        none
 // @run-at       document-idle
@@ -11,7 +11,7 @@
 var dagar = new Array('Mån','Tis','Ons','Tor','Fre','Lör','Sön');
 
 function fixit() {
-	var strng = document.querySelector('.dFdpnc > .eZwnEI:nth-child(2) .duaZGS').textContent; // console.log(strng);
+	var strng = document.querySelector('dl > div:nth-child(2) dd').textContent; // console.log(strng);
 	if (strng.includes('Ikväll') === true) {
 		var datum = new Date();
 	}
