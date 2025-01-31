2d02b03d5c9fe3799f565501211d9b102fa30017
Asral86 <larsm.andersson@gmail.com>
2025-01-31 22:03:35 +0100

    svtplay 1.3.7
    + fler tidskategorier

--- svtplay.user.js
+++ svtplay.user.js
@@ -2,7 +2,7 @@
 // @name         SVT Play
 // @description  Lägg till slutdatum i titel, organisera sista-listan
 // @author       Lars Andersson
-// @version      1.3.6
+// @version      1.3.7
 // @match        https://www.svtplay.se/video/*
 // @match        https://www.svtplay.se/lista/lastchance_start/sista-chansen
 // @grant        none
@@ -13,14 +13,21 @@ if (document.URL.includes('/lastchance_start/sista-chansen')) {
 	function fixlist() {
 		let daddy = document.querySelector('#play_main-content > section > div > section');
 		let klass = daddy.firstChild.classList;
+		let h1div = document.createElement('div'); h1div.classList.add(klass[0], klass[1]); h1div.id = 'h1';
+		let h2div = document.createElement('div'); h2div.classList.add(klass[0], klass[1]); h2div.id = 'h2';
 		let s1div = document.createElement('div'); s1div.classList.add(klass[0], klass[1]); s1div.id = '1d';
 		let s2div = document.createElement('div'); s2div.classList.add(klass[0], klass[1]); s2div.id = '2d';
 		let s3div = document.createElement('div'); s3div.classList.add(klass[0], klass[1]); s3div.id = '3d';
 		let oadiv = document.createElement('div'); oadiv.classList.add(klass[0], klass[1]); oadiv.id = 'oa';
 		let cards = document.querySelectorAll('#play_main-content article[data-css-selector="contentItemCardArticle"]');
-		daddy.appendChild(oadiv); daddy.appendChild(s1div); daddy.appendChild(s2div); daddy.appendChild(s3div);
+		daddy.appendChild(h1div); daddy.appendChild(h2div); daddy.appendChild(oadiv); daddy.appendChild(s1div); daddy.appendChild(s2div); daddy.appendChild(s3div);
 		for (let i = 0; i < cards.length; i++) {
 			if (cards[i].querySelector('[data-testid="play-badge"]') === null) { document.getElementById('3d').appendChild(cards[i]); }
+			else if (cards[i].querySelector('[data-testid="play-badge"]').textContent.includes('timmar kvar') == true)  {
+				let hours = cards[i].querySelector('[data-testid="play-badge"]').textContent.split(' ')[0]; // console.log(hours);
+				if (hours >= 10)     { document.getElementById('h2').appendChild(cards[i]); }
+				else if (hours >= 5) { document.getElementById('h1').appendChild(cards[i]); }
+			}
 			else if (cards[i].querySelector('[data-testid="play-badge"]').textContent.includes('1 dag kvar') == true)   { document.getElementById('1d').appendChild(cards[i]); }
 			else if (cards[i].querySelector('[data-testid="play-badge"]').textContent.includes('Öppet Arkiv') == true)  { document.getElementById('oa').appendChild(cards[i]); }
 			else if (cards[i].querySelector('[data-testid="play-badge"]').textContent.includes('2 dagar kvar') == true) { document.getElementById('2d').appendChild(cards[i]); }
