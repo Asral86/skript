2520bc8eb11fca6ee157bc27358040c7a3de0c21
Asral86 <larsm.andersson@gmail.com>
2026-05-13 21:40:25 +0200

    svt dark theme 1.0

---
+++ svt.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         SVT dark mode
+// @author       Lars Andersson
+// @version      1.0
+// @match        https://www.svt.se/*
+// @grant        none
+// @run-at       document-idle
+// ==/UserScript==
+
+let html = document.documentElement;
+let body = document.body;
+
+if (html.hasAttribute("data-theme")) {
+	window.alert("<html> data-theme: " +  html.getAttribute("data-theme"));
+}
+else if (body.hasAttribute("data-theme")) {
+	window.alert("<body> data-theme: " +  body.getAttribute("data-theme"));
+}
+else { html.setAttribute("data-theme","dark"); }
