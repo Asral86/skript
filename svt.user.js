fdb61aaf58629c0a8d36ca852ac69fefadd9e304
Asral86 <larsm.andersson@gmail.com>
2026-05-31 00:23:35 +0200

    svt 1.0.1

--- svt.user.js
+++ svt.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SVT dark mode
 // @author       Lars Andersson
-// @version      1.0
+// @version      1.0.1
 // @match        https://www.svt.se/*
 // @grant        none
 // @run-at       document-idle
@@ -11,9 +11,9 @@ let html = document.documentElement;
 let body = document.body;
 
 if (html.hasAttribute("data-theme")) {
-	window.alert("<html> data-theme: " +  html.getAttribute("data-theme"));
+	// window.alert("<html> data-theme: " +  html.getAttribute("data-theme"));
 }
 else if (body.hasAttribute("data-theme")) {
-	window.alert("<body> data-theme: " +  body.getAttribute("data-theme"));
+	// window.alert("<body> data-theme: " +  body.getAttribute("data-theme"));
 }
 else { html.setAttribute("data-theme","dark"); }
